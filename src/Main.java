e8828e9f2ea536f974aa30f9966c132943b5cd78
aiger <aigerim.bazarbekova218@gmail.com>
2023-04-27 18:22:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Triangle triangle=new Triangle();
+        triangle.a=3;
+        triangle.b=3;
+        triangle.c=4;
+        triangle.Area();
+
+
+    }
+}
---
+++ src/Triangle.java
@@ -0,0 +1,14 @@
+import java.awt.geom.Area;
+
+public class Triangle {
+    double a;
+    double b;
+    double c;
+
+
+    public void Area(){
+        System.out.println( a*b/2);
+    }
+
+
+}
